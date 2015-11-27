5d89abc92299c300b65f577156b278b921b401bf
Sumit Tomer <sktomer@ncsu.edu>
2015-11-26 21:04:17 -0500

    Controller Synthesis VM Script Files

---
+++ build-vm/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update Ubuntu
+sudo apt-get update
+
+# Install Java and Flash (to view demo videos on YouTube)
+sudo apt-get install -y openjdk-7-jre
+sudo apt-get install -y flashplugin-installer
+
+# Remove unwanted icons from Launcher
+cd /usr/share/applications && sudo rm -f libreoffice-writer.desktop libreoffice-calc.desktop libreoffice-impress.desktop ubuntu-software-center.desktop ubuntu-amazon-default.desktop
+
+# Copy VM files to Desktop
+sudo chmod 777 /vagrant/*
+unzip /vagrant/Controller_Synthesis_VM_Desktop_Files.zip -d /home/vagrant/Desktop/
+chmod +x /home/vagrant/Desktop/*.desktop
+
+# Extract SnakeDemo Java Project to Eclipse workspace
+sudo wget -O /home/vagrant/mtsa-3.6.zip http://sourceforge.net/projects/mtsa/files/latest/download?source=files
+mkdir /home/vagrant/Desktop/mtsa-3.6/
+unzip /home/vagrant/mtsa-3.6.zip -d /home/vagrant/Desktop/mtsa-3.6/
+
+# Add Eclipse to startup applications before reloading VM
+mkdir /home/vagrant/.config/autostart/
+cp /home/vagrant/Desktop/ControllerSynthesis*.desktop /home/vagrant/.config/autostart/
